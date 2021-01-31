abc77fcdad13fa2e73715be8322265d6e40d7e90
dstrauss <fmdenver@gmail.com>
2021-01-31 05:56:18 -0500

    inititial project created

---
+++ SRC/HalfWayToMediocre-WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HalfWayToMediocre_WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
